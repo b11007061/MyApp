6f16bc75381b8f72bfea77db328fde19be9c2680
a <a@a>
2023-10-19 10:44:19 +0800

    1. 優化操作畫面 2. 加入程式碼

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.nio.channels.InterruptedByTimeoutException
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var etName: EditText
+    lateinit var btHi: Button
+    lateinit var tvR: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etName = findViewById(R.id.etName)
+        btHi = findViewById(R.id.btHi)
+        tvR = findViewById(R.id.tvR)
+
+        btHi.setOnClickListener {
+            tvR.text = "你好" + etName.text
+        }
     }
 }
